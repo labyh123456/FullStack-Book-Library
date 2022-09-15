21335a1f4729c14b6d38ac5053f7bcf667910f56
labyh123456 <tommmyy890@gmail.com>
2022-09-15 09:13:43 +0500

    day two

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('hello world can yo do bro right now if you');
 })
-
+console.log('hello dear');
 app.listen(port, ()=> {
     console.log(`Listning on the port at ${port}`);
 });
