c8bfcdf67d135416404b4d36462886b9ac7a5986
labyh123456 <tommmyy890@gmail.com>
2023-03-05 18:05:17 +0500

    complete app

--- app.js
+++ app.js
@@ -1,58 +1,55 @@
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-const express = require('express');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
 const port = process.env.PORT || 8080;
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-
+const expressLayouts = require("express-ejs-layouts");
 
 //configure bodyParser
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 //use method override
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 //import router files
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
-const bookRouter = require('./routes/books')
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
 // set the view engine
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 
 // public files
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // app.get('/', (req, res) => {
 //     res.send('hello world can yo do bro right now if you can help ? because');
 // })
-console.log('hello dear');
+console.log("hello dear");
 
-// import db 
+// import db
 const mongoose = require("mongoose");
 
 mongoose
 
-    .connect(process.env.DATABASE_URL, {
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
 
-        useNewUrlParser: true,
+  .then(() => console.log("Database Connected Successfully ss"))
 
-    })
+  .catch((err) => console.log(err));
 
-    .then(() => console.log("Database Connected Successfully ss"))
-
-    .catch((err) => console.log(err));
-
-
-app.use('/', indexRouter);
-app.use('/authors', authorRouter);
-app.use('/books', bookRouter);
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.listen(port, () => {
-    console.log(`Listning on the port at ${port}`);
+  console.log(`Listning on the port at ${port}`);
 });
